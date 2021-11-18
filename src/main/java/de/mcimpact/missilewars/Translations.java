78590cf052e4c19b01b74a1066ee3c1210265de7
TDR <tdr.minecraft@gmail.com>
2021-11-18 19:06:23 +0100

    Update Translations.java

--- src/main/java/de/mcimpact/missilewars/Translations.java
+++ src/main/java/de/mcimpact/missilewars/Translations.java
@@ -9,8 +9,10 @@ public class Translations {
     public static void run() {
 
         Core.registerTranslation(KEY + "message.movment.playerjoin", "<system#prefix><system#accent>{0} <system#color>joined the game!");
-        Core.registerTranslation(KEY + "message.debug", "<system#prefix> [DEBUG] <system#accent>{0}");
-
-
+        Core.registerTranslation(KEY + "message.debug", "<system#prefix> [DEBUG] <system#color>{0}");
+        Core.registerTranslation(KEY + "message.movement.adminmodejoin", "<system#prefix><system#color>You tried to join a game with AdminMode.");
+        Core.registerTranslation(KEY + "message.movement.playerjoin.failed", "<fatal#prefix><fatal#color>Your join to MissileWars failed!");
+        Core.registerTranslation(KEY + "message.movement.playerjoin.aborted", "<error#prefix><error#color>Your join to MissileWars was aborted!");
+        Core.registerTranslation(KEY + "message.movement.playerjoin.alreadyrunning", "<system#prefix><system#color>This game is already running.");
     }
 }
