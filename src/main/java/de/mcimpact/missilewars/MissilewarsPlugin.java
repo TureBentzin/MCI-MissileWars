8cb063e354b0775f96cdaf22e1359b664ff975fb
TDR <tdr.minecraft@gmail.com>
2021-11-18 16:05:51 +0100

    add Logger

--- src/main/java/de/mcimpact/missilewars/MissilewarsPlugin.java
+++ src/main/java/de/mcimpact/missilewars/MissilewarsPlugin.java
@@ -1,13 +1,19 @@
 package de.mcimpact.missilewars;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
 
 
 public final class MissilewarsPlugin extends JavaPlugin {
 
+    private static final Logger MISSILEWARS_LOGGER = Bukkit.getLogger();
+
     @Override
     public void onEnable() {
         // Plugin startup logic
+        MISSILEWARS_LOGGER.warning("Running MissileWars v." + getDescription().getVersion());
 
     }
 
