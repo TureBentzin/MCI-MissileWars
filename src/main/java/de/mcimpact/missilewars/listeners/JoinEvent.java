5d68dae9922895ee027374e2f31db0e1391edf8f
TDR <tdr.minecraft@gmail.com>
2021-11-18 19:06:57 +0100

    add more messages (incomplete)

--- src/main/java/de/mcimpact/missilewars/listeners/JoinEvent.java
+++ src/main/java/de/mcimpact/missilewars/listeners/JoinEvent.java
@@ -18,16 +18,28 @@ public class JoinEvent implements Listener {
     @EventHandler
     public void onJoin(PlayerJoinEvent e) throws PlayerResolvingException {
 
+        MissileWars.broadcast("missilewars.message.debug", "event: " + e.getEventName());
+
         NetPlayer player = Core.getPlayerUtils().getPlayer(e.getPlayer().getUniqueId());
 
         if(player == null) throw new PlayerResolvingException(e.getPlayer().getUniqueId());
 
         if(MissileWars.GAME.getGameStatus() == GameStatus.LOBBY) {
 
+            if(player.isAdminMode()){
+
+                player.sendMessage(Core.translate(Core.getTranslatableComponent("missilewars.message.movement.playerjoin.failed")));
+                player.sendMessage(Core.translate(Core.getTranslatableComponent("missilewars.message.movement.playerjoin.aborted")));
+
+             return;
+            }
             game.teamer.add(player);
+            MissileWars.broadcast("missilewars.message.movement.playerjoin", player.getName());
+
 
         }else {
 
+            player.sendMessage(Core.translate(Core.getTranslatableComponent("missilewars.message.movement.playerjoin.alreadyrunning")));
 
         }
 
