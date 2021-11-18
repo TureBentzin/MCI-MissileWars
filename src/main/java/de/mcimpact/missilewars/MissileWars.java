8fbe80a413a70ba493c54812d61531a7c9660b09
TDR <tdr.minecraft@gmail.com>
2021-11-18 19:06:38 +0100

    add broadcasting

--- src/main/java/de/mcimpact/missilewars/MissileWars.java
+++ src/main/java/de/mcimpact/missilewars/MissileWars.java
@@ -3,6 +3,7 @@ package de.mcimpact.missilewars;
 import com.onarandombox.MultiverseCore.MultiverseCore;
 import de.mcimpact.core.Core;
 import de.mcimpact.missilewars.game.MissileWarsGame;
+import de.mcimpact.missilewars.listeners.JoinEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -14,23 +15,24 @@ public final class MissileWars extends JavaPlugin {
     private static final Logger MISSILEWARS_LOGGER = Bukkit.getLogger();
     private static MultiverseCore multiverse = null;
     public static MissileWarsGame GAME = MissileWarsGame.getInstance();
-    public static void broadcast(String... keysAndArgs){
-        String[] args = new String[keysAndArgs.length -1];
 
-        for (int i = 1; i < keysAndArgs.length; i++) {
-          args[i] = keysAndArgs[i];
-        }
-
-        Bukkit.broadcast(Core.getTranslatableComponent(keysAndArgs[0], args));
+    public static void broadcast(String key, String... args){
+        Bukkit.broadcast(Core.translate(Core.getTranslatableComponent(key, args)));
+    }
+    public static void broadcast(String key){
+        Bukkit.broadcast(Core.translate(Core.getTranslatableComponent(key)));
     }
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         MISSILEWARS_LOGGER.warning("Running MissileWars v." + getDescription().getVersion());
+        MISSILEWARS_LOGGER.info("registering Translations");
+        Translations.run();
         multiverse = (MultiverseCore) Bukkit.getServer().getPluginManager().getPlugin("Multiverse-Core");
-
-
+        MISSILEWARS_LOGGER.info("stating listening to events!");
+        registerListeners();
+        MISSILEWARS_LOGGER.info("all events are registered now!");
 
     }
 
@@ -39,6 +41,10 @@ public final class MissileWars extends JavaPlugin {
         // Plugin shutdown logic
     }
 
+
+    public void registerListeners() {
+        Bukkit.getPluginManager().registerEvents(new JoinEvent(), this);
+    }
 
 
 }
