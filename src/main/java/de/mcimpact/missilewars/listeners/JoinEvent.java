bfce6539c445ef0428f9148b474c5bcc1e863ff9
TDR <tdr.minecraft@gmail.com>
2021-11-18 17:36:55 +0100

    Update JoinEvent.java

--- src/main/java/de/mcimpact/missilewars/listeners/JoinEvent.java
+++ src/main/java/de/mcimpact/missilewars/listeners/JoinEvent.java
@@ -2,14 +2,17 @@ package de.mcimpact.missilewars.listeners;
 
 import de.mcimpact.core.Core;
 import de.mcimpact.core.players.NetPlayer;
-import de.mcimpact.missilewars.MissileWarsPlugin;
+import de.mcimpact.missilewars.MissileWars;
 import de.mcimpact.missilewars.errors.PlayerResolvingException;
 import de.mcimpact.missilewars.game.GameStatus;
+import de.mcimpact.missilewars.game.MissileWarsGame;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class JoinEvent implements Listener {
+
+    MissileWarsGame game = MissileWarsGame.getInstance();
 
 
     @EventHandler
@@ -19,11 +22,9 @@ public class JoinEvent implements Listener {
 
         if(player == null) throw new PlayerResolvingException(e.getPlayer().getUniqueId());
 
-        if(MissileWarsPlugin.GAME.getGameStatus() == GameStatus.LOBBY) {
+        if(MissileWars.GAME.getGameStatus() == GameStatus.LOBBY) {
 
-
-
-
+            game.teamer.add(player);
 
         }else {
 
