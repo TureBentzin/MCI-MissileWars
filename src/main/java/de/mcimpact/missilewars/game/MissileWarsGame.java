da51add4b1108ae0ac7dcd3081645353ef6fb31b
TDR <tdr.minecraft@gmail.com>
2021-11-18 16:52:32 +0100

    Update MissileWarsGame.java

--- src/main/java/de/mcimpact/missilewars/game/MissileWarsGame.java
+++ src/main/java/de/mcimpact/missilewars/game/MissileWarsGame.java
@@ -5,18 +5,25 @@ import de.mcimpact.core.game.Game;
 public class MissileWarsGame extends Game {
 
     private boolean running;
-
     public boolean isRunning() {
         return running;
     }
-
     private void setRunning(boolean running) {
         this.running = running;
     }
 
+    private GameStatus gameStatus = GameStatus.PAUSED;
+    public GameStatus getGameStatus() {
+        return gameStatus;
+    }
+    public void setGameStatus(GameStatus gameStatus) {
+        this.gameStatus = gameStatus;
+    }
+
+
     @Override
     public  void start() {
-
+        setGameStatus(GameStatus.LOBBY);
 
         internalStart("servername");
     }
