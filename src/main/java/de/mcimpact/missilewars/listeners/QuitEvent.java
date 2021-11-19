41431a8f89bd57e365cf16a1d4c727b77e864bc7
TDR <tdr.minecraft@gmail.com>
2021-11-19 14:27:43 +0100

    Create QuitEvent.java

---
+++ src/main/java/de/mcimpact/missilewars/listeners/QuitEvent.java
@@ -0,0 +1,24 @@
+package de.mcimpact.missilewars.listeners;
+
+import de.mcimpact.core.Core;
+import de.mcimpact.core.players.NetPlayer;
+import de.mcimpact.missilewars.MissileWars;
+import de.mcimpact.missilewars.errors.PlayerResolvingException;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+import java.net.http.WebSocket;
+
+public class QuitEvent implements Listener {
+
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent e) throws PlayerResolvingException {
+        MissileWars.broadcast("missilewars.message.debug", "event: " + e.getEventName());
+
+        NetPlayer player = Core.getPlayerUtils().getPlayer(e.getPlayer().getUniqueId());
+
+        if(player == null) throw new PlayerResolvingException(e.getPlayer().getUniqueId());
+    }
+}
