4964e6bdd83c599cc598623f1f525583a1288fc0
TDR <tdr.minecraft@gmail.com>
2021-11-20 18:27:04 +0100

    Update FlightAttempt.java

--- src/main/java/de/mcimpact/missilewars/listeners/FlightAttempt.java
+++ src/main/java/de/mcimpact/missilewars/listeners/FlightAttempt.java
@@ -1,5 +1,9 @@
 package de.mcimpact.missilewars.listeners;
 
+import de.mcimpact.core.Core;
+import de.mcimpact.core.paper.LocalPlayer;
+import de.mcimpact.core.util.Utils;
+import de.mcimpact.missilewars.MissileWars;
 import org.bukkit.GameMode;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,13 +14,21 @@ import org.bukkit.util.Vector;
 public class FlightAttempt implements Listener {
 
     @EventHandler
+    @SuppressWarnings("deprecated")
     public void onFlightAttempt(PlayerToggleFlightEvent event) {
-
+        MissileWars.broadcast("missilewars.message.debug", "flight attempt: " + event.getPlayer().getCustomName());
         if (!event.getPlayer().isFlying() && event.getPlayer().getGameMode() != GameMode.CREATIVE) {
+            if(Utils.isOnGround(Core.getPlayerUtils().getPlayer(event.getPlayer().getUniqueId()))) {
+                MissileWars.broadcast("missilewars.message.debug", "double jump: " + event.getPlayer().getCustomName());
+                Vector vector = event.getPlayer().getVelocity();
 
-            event.getPlayer().setVelocity(event.getPlayer().getVelocity().add(new Vector(0, 3, 0)));
+                vector.add(new Vector(0, 0.5, 0));
+
+                event.getPlayer().setVelocity(event.getPlayer().getVelocity().add(vector));
+
+            }
+            event.getPlayer().setFlying(false);
             event.setCancelled(true);
-
         }
 
     }
