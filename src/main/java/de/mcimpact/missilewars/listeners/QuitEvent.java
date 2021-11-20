033ac3f883deab79a3e92e5647d38bf05bb5a82b
TDR <tdr.minecraft@gmail.com>
2021-11-20 12:37:39 +0100

    update core

--- src/main/java/de/mcimpact/missilewars/listeners/QuitEvent.java
+++ src/main/java/de/mcimpact/missilewars/listeners/QuitEvent.java
@@ -4,11 +4,15 @@ import de.mcimpact.core.Core;
 import de.mcimpact.core.players.NetPlayer;
 import de.mcimpact.missilewars.MissileWars;
 import de.mcimpact.missilewars.errors.PlayerResolvingException;
+import de.mcimpact.missilewars.game.GameStatus;
+import de.mcimpact.missilewars.game.MissileWarsGame;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class QuitEvent implements Listener {
+
+    MissileWarsGame game = MissileWarsGame.getInstance();
 
 
     @EventHandler
@@ -18,5 +22,16 @@ public class QuitEvent implements Listener {
         NetPlayer player = Core.getPlayerUtils().getPlayer(e.getPlayer().getUniqueId());
 
         if (player == null) throw new PlayerResolvingException(e.getPlayer().getUniqueId());
+
+        //Take Action
+
+        if(game.getGameStatus() == GameStatus.LOBBY) {
+            game.teamer.remove(player);
+            player.sendMessage(Core.getTranslatableComponent("missilewars.message.movement.playerquit", player.getName()));
+        }
+
+
     }
+
+
 }
