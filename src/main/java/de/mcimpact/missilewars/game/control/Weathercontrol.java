18cf37cfd09762d0193d4b218db416f4672c1002
TDR <tdr.minecraft@gmail.com>
2021-11-20 15:58:18 +0100

    Update Weathercontrol.java

--- src/main/java/de/mcimpact/missilewars/game/control/Weathercontrol.java
+++ src/main/java/de/mcimpact/missilewars/game/control/Weathercontrol.java
@@ -1,9 +1,14 @@
 package de.mcimpact.missilewars.game.control;
 
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+
 public class Weathercontrol {
 
     public void setRain(boolean b) {
-
+        for (World world : Bukkit.getWorlds()) {
+            world.setStorm(b);
+        }
     }
 
 }
