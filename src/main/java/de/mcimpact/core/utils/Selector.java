41a8bae0866ae8216ea6dd082c067ed8d8f08664
Bommels05 <69975756+Bommels05@users.noreply.github.com>
2021-11-20 17:48:44 +0100

    Basic structure
    Added Consturctors and Variables

--- src/main/java/de/mcimpact/core/utils/Selector.java
+++ src/main/java/de/mcimpact/core/utils/Selector.java
@@ -3,18 +3,30 @@ package de.mcimpact.core.utils;
 import de.mcimpact.core.players.NetPlayer;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Selector<R, E extends Selector.SelectorEntry> {
+    List<SelectorEntry> entries;
+
+    public Selector(List<SelectorEntry> entries) {
+        this.entries = entries;
+    }
 
     public void open(NetPlayer netPlayer){
         //TODO: open
     }
 
-    protected abstract R handle(E entry);
+    protected abstract void handle(E entry);
 
 
 
     public static class SelectorEntry{
-        ItemStack entryStack;
+        ItemStack item;
+
+        public SelectorEntry(ItemStack item) {
+            this.item = item;
+        }
 
         public void onClick() {
             
