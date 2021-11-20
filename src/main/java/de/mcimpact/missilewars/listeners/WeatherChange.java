9621607306a7399607a72d6b7469c7f2c7fdf707
TDR <tdr.minecraft@gmail.com>
2021-11-20 17:35:09 +0100

    add WeatherChange

---
+++ src/main/java/de/mcimpact/missilewars/listeners/WeatherChange.java
@@ -0,0 +1,17 @@
+package de.mcimpact.missilewars.listeners;
+
+import de.mcimpact.missilewars.game.control.Weathercontrol;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.weather.WeatherChangeEvent;
+
+
+public class WeatherChange implements Listener {
+
+
+    @EventHandler
+    public void onChange(WeatherChangeEvent event){
+        if(event.toWeatherState() && Weathercontrol.allow_storm)
+        event.setCancelled(true);
+    }
+}
