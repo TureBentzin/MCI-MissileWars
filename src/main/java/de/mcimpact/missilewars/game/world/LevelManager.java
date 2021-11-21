b2963368a73a633e9193e41d1ce953457bd81b1f
TDR <tdr.minecraft@gmail.com>
2021-11-21 10:29:00 +0100

    add getRandomLevel

--- src/main/java/de/mcimpact/missilewars/game/world/LevelManager.java
+++ src/main/java/de/mcimpact/missilewars/game/world/LevelManager.java
@@ -2,6 +2,7 @@ package de.mcimpact.missilewars.game.world;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class LevelManager {
 
@@ -11,5 +12,17 @@ public class LevelManager {
         return missileWarsLevelMap;
     }
 
+    public MissileWarsLevel getRandomLevel() {
 
+        String[] strings = new String[missileWarsLevelMap.keySet().size()];
+
+        for (int i = 0; i < missileWarsLevelMap.keySet().size(); i++) {
+            strings[i] = (String) missileWarsLevelMap.keySet().toArray()[i];
+        }
+
+        Random r = new Random();
+        int randomNumber = r.nextInt(strings.length);
+
+        return missileWarsLevelMap.get(strings[randomNumber]);
+    }
 }
