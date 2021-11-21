184f766024a09eab0316d33af6ae331c667ce0bf
TDR <tdr.minecraft@gmail.com>
2021-11-21 13:36:16 +0100

    add scanForLevels()

--- src/main/java/de/mcimpact/missilewars/game/world/LevelManager.java
+++ src/main/java/de/mcimpact/missilewars/game/world/LevelManager.java
@@ -1,5 +1,9 @@
 package de.mcimpact.missilewars.game.world;
 
+import de.mcimpact.core.util.Utils;
+import org.bukkit.Bukkit;
+
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
@@ -24,5 +28,20 @@ public class LevelManager {
         int randomNumber = r.nextInt(strings.length);
 
         return missileWarsLevelMap.get(strings[randomNumber]);
+    }
+
+
+    public void scanForLevels() {
+        File folder = Bukkit.getServer().getWorldContainer();
+
+        File[] potentialLevels = Utils.stringArrayToFolders(folder.getPath(), Utils.getSubdirectories(folder.getPath()));
+
+        for (File potentialLevel : potentialLevels) {
+            for (File file : potentialLevel.listFiles()) {
+                if (file.getName() == "thisIsAWorld.tdrstudios") {
+                    getMissileWarsLevelMap().put(potentialLevel.getName(), new MissileWarsLevel());
+                }
+            }
+        }
     }
 }
