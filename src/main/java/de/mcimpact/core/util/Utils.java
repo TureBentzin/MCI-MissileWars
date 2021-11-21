b08baa22bed031375b776e743a31b99d5587d161
TDR <tdr.minecraft@gmail.com>
2021-11-21 12:54:07 +0100

    add File indexer

--- src/main/java/de/mcimpact/core/util/Utils.java
+++ src/main/java/de/mcimpact/core/util/Utils.java
@@ -6,6 +6,9 @@ import org.bukkit.Material;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 
+import java.io.File;
+import java.io.FilenameFilter;
+
 public class Utils {
 
     public static boolean isOnGround(NetPlayer netPlayer) {
@@ -13,5 +16,20 @@ public class Utils {
         Player player = Bukkit.getPlayer(netPlayer.getUniqueId());
 
         return !player.getLocation().getBlock().getRelative(BlockFace.DOWN).getType().equals(Material.AIR);
+    }
+
+    public static String[] getSubdirectories(String path) {
+        File file = new File(path);
+        String[] directories = file.list((current, name) -> new File(current, name).isDirectory());
+        return directories;
+    }
+
+    public static File[] stringArrayToFolders(String path, String[] folders) {
+
+        File[] files = new File[folders.length];
+        for (int i = 0; i < folders.length; i++) {
+            files[i] = new File(path + "/" + folders[i]);
+        }
+        return files;
     }
 }
