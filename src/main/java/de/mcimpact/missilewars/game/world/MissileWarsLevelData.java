65630df6726d2c2d801bb9c53acb0990dafa54bc
TDR <tdr.minecraft@gmail.com>
2021-11-21 10:53:33 +0100

    Update MissileWarsLevelData.java

--- src/main/java/de/mcimpact/missilewars/game/world/MissileWarsLevelData.java
+++ src/main/java/de/mcimpact/missilewars/game/world/MissileWarsLevelData.java
@@ -1,4 +1,48 @@
 package de.mcimpact.missilewars.game.world;
 
-public class MissileWarsLevelData {
+import kotlin.Pair;
+import org.bukkit.Location;
+
+import java.io.Serializable;
+
+public class MissileWarsLevelData implements Serializable {
+
+    public MissileWarsLevelData(String levelname, LocationPair spawnLocationPair, LocationPair portalCheckLocations, int size){
+        this.levelname = levelname;
+        this.spawnLocationPair = spawnLocationPair;
+        this.portalCheckLocations = portalCheckLocations;
+        this.size = size;
+    }
+
+    private String levelname;
+
+    /**
+     * spawnLocation of team 1 and 2
+     */
+    private LocationPair spawnLocationPair;
+
+    /**
+     * Where is portal is checked for each team
+     */
+    private LocationPair portalCheckLocations;
+
+    private int size = 1;
+
+
+    public int getSize() {
+        return size;
+    }
+
+
+    public LocationPair getPortalCheckLocations() {
+        return portalCheckLocations;
+    }
+
+    public LocationPair getSpawnLocationPair() {
+        return spawnLocationPair;
+    }
+
+    public String getLevelname() {
+        return levelname;
+    }
 }
