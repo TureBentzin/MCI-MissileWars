a2db5cb535049b16d959f3e77d666711c1e5b69d
TDR <tdr.minecraft@gmail.com>
2021-11-23 13:55:19 +0100

    Update TNTExplosion.java

--- src/main/java/de/mcimpact/missilewars/listeners/TNTExplosion.java
+++ src/main/java/de/mcimpact/missilewars/listeners/TNTExplosion.java
@@ -3,15 +3,37 @@ package de.mcimpact.missilewars.listeners;
 import com.destroystokyo.paper.event.block.TNTPrimeEvent;
 import de.mcimpact.core.util.Utils;
 import de.mcimpact.missilewars.MissileWars;
+import de.mcimpact.missilewars.game.GameStatus;
 import org.bukkit.entity.TNTPrimed;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityExplodeEvent;
 
 public class TNTExplosion  implements Listener {
 
 
     @EventHandler
-    public void onExplode(TNTPrimeEvent event) {
-        MissileWars.broadcast("missilewars.message.debug","at:" + Utils.getCoordsFromBlock(event.getBlock()));
+    public void onPrime(TNTPrimeEvent event) {
+        MissileWars.broadcast("missilewars.message.debug","Primed at:" + Utils.getCoordsFromBlock(event.getBlock()));
+        if(MissileWars.GAME.getGameStatus() == GameStatus.GAME){
+
+
+
+        }else {
+
+            event.setCancelled(true);
+
+        }
+    }
+
+    @EventHandler
+    public void onExplode(EntityExplodeEvent event) {
+        if(MissileWars.GAME.getGameStatus() == GameStatus.GAME) {
+            MissileWars.broadcast("missilewars.message.debug","Explosion ("+ event.getEntityType().name()+") at:" + Utils.getCoordsFromBlock(event.getEntity().getLocation().getBlock()));
+
+
+        }else {
+            event.setCancelled(true);
+        }
     }
 }
