0e4b0f3084497a4aa692042e47d66ee9bb35efe9
TDR <tdr.minecraft@gmail.com>
2021-11-24 14:28:55 +0100

    ahhh kotlin sucks!

--- src/main/java/de/mcimpact/missilewars/listeners/JoinEvent.java
+++ src/main/java/de/mcimpact/missilewars/listeners/JoinEvent.java
@@ -51,7 +51,7 @@ public class JoinEvent implements Listener {
             player.sendMessage(Core.getTranslatableComponent("missilewars.message.teaming.joined",
                     Component.text(game.teamer.getTeam(player).getColor().name()).color(game.teamer.getTeam(player).getColor().getTextColor().adventure)));
             System.out.println("ready: " + LobbyPhase.checkForStart());
-            Bukkit.getScheduler().runTaskAsynchronously(MissileWars.getMissileWars(), () -> MissileWars.GAME.start());
+            Bukkit.getScheduler().runTaskAsynchronously(MissileWars.getMissileWars(), () -> LobbyPhase.getStartTimer().start());
             //  MissileWars.broadcast("missilewars.message.debug", game.teamer.getTeamMap().toString());
 
 
