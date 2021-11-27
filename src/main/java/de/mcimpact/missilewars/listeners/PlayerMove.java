5ac0fb17680641e7f6ca7703abadf6beaefb8b68
TDR <tdr.minecraft@gmail.com>
2021-11-27 15:06:45 +0100

    add contact

--- src/main/java/de/mcimpact/missilewars/listeners/PlayerMove.java
+++ src/main/java/de/mcimpact/missilewars/listeners/PlayerMove.java
@@ -1,9 +1,16 @@
 package de.mcimpact.missilewars.listeners;
 
+import de.mcimpact.core.Core;
+import de.mcimpact.core.game.Game;
+import de.mcimpact.core.players.NetPlayer;
+import de.mcimpact.gamephase.GamePhase;
 import de.mcimpact.missilewars.MissileWars;
 import de.mcimpact.missilewars.game.GameStatus;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 public class PlayerMove implements Listener {
@@ -15,11 +22,12 @@ public class PlayerMove implements Listener {
         event.getPlayer().setAllowFlight(true);
         }else if(MissileWars.GAME.getGameStatus() == GameStatus.GAME) {
             //GamePhase stuff
-
-
-
+            NetPlayer netPlayer = Core.getPlayerUtils().getPlayer(event.getPlayer().getUniqueId());
+            if(GamePhase.hasContact(netPlayer, Material.WATER) && MissileWars.GAME.isPlayingPlayer(netPlayer)) {
+                GamePhase.killPlayer(new PlayerKill.KillInformation(netPlayer, Core.translate(Core.getTranslatableComponent("missilewars.message.died", netPlayer.getName())),
+                        null, event.getPlayer().getLocation(), EntityDamageEvent.DamageCause.CONTACT));
+            }
         }
-
 
         if (event.getPlayer().isOnGround())
             FlightAttempt.uuidSet.add(event.getPlayer().getUniqueId());
