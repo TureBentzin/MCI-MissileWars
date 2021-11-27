e4285225fb3fa3088924b1ab02e3aaf1a7583f09
Bommels05 <69975756+Bommels05@users.noreply.github.com>
2021-11-27 15:18:10 +0100

    Add Logic

--- src/main/java/de/mcimpact/missilewars/listeners/PlayerMove.java
+++ src/main/java/de/mcimpact/missilewars/listeners/PlayerMove.java
@@ -1,5 +1,7 @@
 package de.mcimpact.missilewars.listeners;
 
+import de.mcimpact.missilewars.MissileWars;
+import de.mcimpact.missilewars.game.GameStatus;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
@@ -9,10 +11,28 @@ public class PlayerMove implements Listener {
     @EventHandler
     public void onMove(PlayerMoveEvent event) {
        // event.getPlayer().sendActionBar("ground:" + event.getPlayer().isOnGround());
-        if(Missilewars.GAME.getGameStatus() == GameStatus.LOBBY) {
+        if(MissileWars.GAME.getGameStatus() == GameStatus.LOBBY) {
         event.getPlayer().setAllowFlight(true);
         }
-        if (event.getPlayer().isOnGround())
+        if (event.getPlayer().isOnGround()) {
             FlightAttempt.uuidSet.add(event.getPlayer().getUniqueId());
+        }
+        if (MissileWars.GAME.getGameStatus() == GameStatus.GAME) {
+            if (event.getPlayer().getLocation().getX() <= MissileWars.GAME.getMissileWarsLevel().getData().getWallsX().getFirst()) {
+                event.setCancelled(true);
+            }
+            if (event.getPlayer().getLocation().getX() >= MissileWars.GAME.getMissileWarsLevel().getData().getWallsX().getSecond()) {
+                event.setCancelled(true);
+            }
+            if (event.getPlayer().getLocation().getZ() <= MissileWars.GAME.getMissileWarsLevel().getData().getWallsZ().getFirst()) {
+                event.setCancelled(true);
+            }
+            if (event.getPlayer().getLocation().getZ() >= MissileWars.GAME.getMissileWarsLevel().getData().getWallsZ().getSecond()) {
+                event.setCancelled(true);
+            }
+            if (event.getPlayer().getLocation().getY() >= MissileWars.GAME.getMissileWarsLevel().getData().getWallY()) {
+                event.setCancelled(true);
+            }
+        }
     }
 }
