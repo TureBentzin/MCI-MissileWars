a169caf7624853131f33651a3541a077eab00419
TDR <tdr.minecraft@gmail.com>
2021-11-27 14:06:02 +0100

    Update PlayerMove.java

--- src/main/java/de/mcimpact/missilewars/listeners/PlayerMove.java
+++ src/main/java/de/mcimpact/missilewars/listeners/PlayerMove.java
@@ -1,5 +1,7 @@
 package de.mcimpact.missilewars.listeners;
 
+import de.mcimpact.missilewars.MissileWars;
+import de.mcimpact.missilewars.game.GameStatus;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
@@ -9,7 +11,7 @@ public class PlayerMove implements Listener {
     @EventHandler
     public void onMove(PlayerMoveEvent event) {
        // event.getPlayer().sendActionBar("ground:" + event.getPlayer().isOnGround());
-        if(Missilewars.GAME.getGameStatus() == GameStatus.LOBBY) {
+        if(MissileWars.GAME.getGameStatus() == GameStatus.LOBBY) {
         event.getPlayer().setAllowFlight(true);
         }
         if (event.getPlayer().isOnGround())
