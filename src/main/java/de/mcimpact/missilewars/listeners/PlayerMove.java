621c8661999cd4b51b818d49c3b1d229e056736c
TDR <38729194+TDRMinecraft@users.noreply.github.com>
2021-11-27 13:31:14 +0100

    Update src/main/java/de/mcimpact/missilewars/listeners/PlayerMove.java
    Co-authored-by: Bommels05 <69975756+Bommels05@users.noreply.github.com>

--- src/main/java/de/mcimpact/missilewars/listeners/PlayerMove.java
+++ src/main/java/de/mcimpact/missilewars/listeners/PlayerMove.java
@@ -9,7 +9,9 @@ public class PlayerMove implements Listener {
     @EventHandler
     public void onMove(PlayerMoveEvent event) {
        // event.getPlayer().sendActionBar("ground:" + event.getPlayer().isOnGround());
+        if(Missilewars.GAME.getGameStatus() == GameStatus.LOBBY) {
         event.getPlayer().setAllowFlight(true);
+        }
         if (event.getPlayer().isOnGround())
             FlightAttempt.uuidSet.add(event.getPlayer().getUniqueId());
     }
