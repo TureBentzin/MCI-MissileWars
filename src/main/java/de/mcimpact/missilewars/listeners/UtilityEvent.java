7af59db6e2df3e6245ac56a5c89743bad82c747a
TDR <tdr.minecraft@gmail.com>
2021-12-04 18:11:10 +0100

    Create UtilityEvent.java

---
+++ src/main/java/de/mcimpact/missilewars/listeners/UtilityEvent.java
@@ -0,0 +1,26 @@
+package de.mcimpact.missilewars.listeners;
+
+import de.mcimpact.core.Core;
+import de.mcimpact.missilewars.MissileWars;
+import de.mcimpact.missilewars.game.GameStatus;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.server.ServerListPingEvent;
+
+import java.net.http.WebSocket;
+
+public class UtilityEvent implements Listener {
+
+    @EventHandler
+    public void onServerListPing(ServerListPingEvent event) {
+       if(MissileWars.GAME.getGameStatus() == GameStatus.LOBBY) {
+           event.motd(Core.translate(Core.getTranslatableComponent("missilewars.motd.lobby")));
+       }else if(MissileWars.GAME.isRunning()) {
+           event.motd(Core.translate(Core.getTranslatableComponent("missilewars.motd.game")));
+       }else if(MissileWars.GAME.getGameStatus() == GameStatus.GAME_END) {
+           event.motd(Core.translate(Core.getTranslatableComponent("missilewars.motd.end")));
+       }else if(MissileWars.GAME.getGameStatus() == GameStatus.PAUSED) {
+           event.motd(Core.translate(Core.getTranslatableComponent("missilewars.motd.paused")));
+       }
+    }
+}
