4d07931e64b456e26e9a15cd656fbe39cadcdb1c
TDR <tdr.minecraft@gmail.com>
2021-12-04 09:39:45 +0100

    Create SimpleReceivableItem.java

---
+++ src/main/java/de/mcimpact/missilewars/game/items/SimpleReceivableItem.java
@@ -0,0 +1,24 @@
+package de.mcimpact.missilewars.game.items;
+
+import net.kyori.adventure.text.Component;
+import org.bukkit.Material;
+
+import java.util.List;
+
+public class SimpleReceivableItem extends SimpleItem implements ReceivableItem{
+
+    private final double percentage;
+
+    public SimpleReceivableItem(Material material, Component name, List<Component> components, double percentage) {
+        super(material, name, components);
+        this.percentage = percentage;
+    }
+
+    /**
+     * @return value between 0 and 1
+     */
+    @Override
+    public double getPercentage() {
+        return percentage;
+    }
+}
