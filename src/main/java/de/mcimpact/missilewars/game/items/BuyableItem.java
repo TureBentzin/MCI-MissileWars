8b1fb38a6c7fd7f9e1c6b347391dd5262b410fc3
TDR <tdr.minecraft@gmail.com>
2021-12-04 08:02:13 +0100

    Create BuyableItem.java

---
+++ src/main/java/de/mcimpact/missilewars/game/items/BuyableItem.java
@@ -0,0 +1,15 @@
+package de.mcimpact.missilewars.game.items;
+
+import de.mcimpact.core.players.NetPlayer;
+
+public interface BuyableItem extends Item{
+
+    ItemWorth buyWorth();
+
+    /**
+     *
+     * @param buyer - The NetPlayer who is trying to buy to Item
+     * @return <code>true</code> is the Item was purchased and <code>false</code> if not!
+     */
+    boolean buy(NetPlayer buyer);
+}
