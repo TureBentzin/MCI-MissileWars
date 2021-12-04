40a42dce852d9e7acd65b3a58434ba80fb906212
TDR <tdr.minecraft@gmail.com>
2021-12-04 07:53:25 +0100

    Create SellableItem.java

---
+++ src/main/java/de/mcimpact/missilewars/game/items/SellableItem.java
@@ -0,0 +1,13 @@
+package de.mcimpact.missilewars.game.items;
+
+import de.mcimpact.core.players.NetPlayer;
+
+public interface SellableItem extends Item{
+
+    /**
+     *
+     * @param seller the NetPlayer who sold the Item
+     * @return the worth the item was sold for
+     */
+    int sell(NetPlayer seller);
+}
