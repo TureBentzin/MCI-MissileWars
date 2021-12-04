c7431b592e19ea5984607d4aa7ffbb86a48cc405
TDR <tdr.minecraft@gmail.com>
2021-12-04 08:29:01 +0100

    add handle()

--- src/main/java/de/mcimpact/missilewars/game/items/ItemManager.java
+++ src/main/java/de/mcimpact/missilewars/game/items/ItemManager.java
@@ -1,5 +1,14 @@
 package de.mcimpact.missilewars.game.items;
 
+import de.mcimpact.missilewars.MissileWars;
+import org.bukkit.event.Event;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public final class ItemManager {
 
     private static final ItemManager INSTANCE = new ItemManager();
@@ -7,5 +16,25 @@ public final class ItemManager {
         return INSTANCE;
     }
 
+    private Set<Item> items = new HashSet<>();
 
+    public Set<Item> getItems() {
+        return items;
+    }
+
+    private void handle(Event event) {
+
+
+        if(event instanceof PlayerInteractEvent) {
+            if(!MissileWars.GAME.isPlayingPlayer(((PlayerEvent) event).getPlayer().getUniqueId())) return;
+            items.forEach(item -> { if(item instanceof InteractableItem)
+                ((InteractableItem) item).onInteract((PlayerInteractEvent) event); });
+        }
+        if(event instanceof InventoryClickEvent) {
+            InventoryClickEvent inventoryClickEvent = (InventoryClickEvent) event;
+             if(!MissileWars.GAME.isPlayingPlayer(((InventoryClickEvent) event).getWhoClicked().getUniqueId()));
+            items.forEach(item -> { if(item instanceof UsableItem)
+                ((UsableItem) item).onClick(inventoryClickEvent); });
+        }
+    }
 }
