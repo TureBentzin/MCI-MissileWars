c747a275c9970ac8f60af6dd3b17a0494822086c
TDR <tdr.minecraft@gmail.com>
2021-12-06 08:19:47 +0100

    generateID

--- src/main/java/de/mcimpact/core/util/Identifiable.java
+++ src/main/java/de/mcimpact/core/util/Identifiable.java
@@ -1,9 +1,23 @@
 package de.mcimpact.core.util;
 
 public interface Identifiable {
+
+    String UTF = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~  "  ;                              ¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ
+    String ALLOWED = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_";
+
     /**
      *
-     * @return ID that instances of <code>"abcdefghijklmnopqrstuvwxyz1234567890_-,.+#"</code>
+     * @return ID that instances of <code>"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_"</code>
      */
     String getID();
+
+
+    static String generateID(String s) {
+        s.replace(" ", "_");
+        for (char c : UTF.toCharArray()) {
+            if(!ALLOWED.contains(s))
+                s.replace(c + "", "");
+        }
+        return s;
+    }
 }
