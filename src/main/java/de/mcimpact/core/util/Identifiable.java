f44e10c380c416fef38c4250537294de76103954
TDR <tdr.minecraft@gmail.com>
2021-12-06 08:10:30 +0100

    Create Identifiable.java

---
+++ src/main/java/de/mcimpact/core/util/Identifiable.java
@@ -0,0 +1,9 @@
+package de.mcimpact.core.util;
+
+public interface Identifiable {
+    /**
+     *
+     * @return ID that instances of <code>"abcdefghijklmnopqrstuvwxyz1234567890_-,.+#"</code>
+     */
+    String getID();
+}
