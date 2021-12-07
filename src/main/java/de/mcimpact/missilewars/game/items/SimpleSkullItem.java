ed9eb79068cbc55d63cb7bc5faa8421aac46e119
TDR <tdr.minecraft@gmail.com>
2021-12-07 17:14:17 +0100

    add Translation feature

--- src/main/java/de/mcimpact/missilewars/game/items/SimpleSkullItem.java
+++ src/main/java/de/mcimpact/missilewars/game/items/SimpleSkullItem.java
@@ -1,7 +1,9 @@
 package de.mcimpact.missilewars.game.items;
 
+import de.mcimpact.core.internal.Untested;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.List;
 
@@ -16,6 +18,15 @@ public abstract class SimpleSkullItem extends SimpleItem implements SkullItem{
 
     public SimpleSkullItem(Component name, int amount) {
         super(Material.PLAYER_HEAD, name, amount);
+    }
+
+    /**
+     * @param key valid Translationkey
+     * @implNote Use <code>{@link SimpleSkullItem#SimpleSkullItem(Component, List, int)}, {@link SimpleSkullItem#SimpleSkullItem(Component, Component, int)} or {@link SimpleSkullItem#SimpleSkullItem(Component, int)} instead!</code>
+     */
+    public SimpleSkullItem(String key, int amount) {
+        super(SimpleTranslatableItem.generateTranslatableStack(Material.PLAYER_HEAD,key, amount), amount);
+
     }
 
 }
