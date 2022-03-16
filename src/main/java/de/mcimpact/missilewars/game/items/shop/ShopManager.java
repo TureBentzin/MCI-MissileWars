0653269656825b30fa989f8b24003ade7b37f5a6
TDR <tdr.minecraft@gmail.com>
2022-03-16 12:20:42 +0100

    Create ShopManager.java

---
+++ src/main/java/de/mcimpact/missilewars/game/items/shop/ShopManager.java
@@ -0,0 +1,26 @@
+package de.mcimpact.missilewars.game.items.shop;
+
+import de.mcimpact.core.util.Pair;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+
+public final class ShopManager implements Listener {
+
+    public ShopManager() {
+
+    }
+
+    private Pair<Shop> shopPair;
+
+
+    @EventHandler
+    public void onClick(PlayerInteractEntityEvent interactEvent) {
+        if(shopPair.getFirst().getKeeper().equals(interactEvent.getRightClicked())) {
+            interactEvent.setCancelled(true);
+            shopPair.getFirst().open(interactEvent.getPlayer());
+        }
+    }
+
+
+}
